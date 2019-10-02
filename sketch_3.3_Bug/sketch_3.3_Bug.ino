6fef42f1462fb3cda350a6fbd3c070de915f77e4
Nobodygotu00 <kelson.reggie@gmail.com>
2019-10-02 10:19:39 -0600

    Copied and Pasted

--- sketch_3.3_Bug/sketch_3.3_Bug.ino
+++ sketch_3.3_Bug/sketch_3.3_Bug.ino
@@ -2,22 +2,29 @@
 
 const int TrigPin = 2;
 const int EchoPin = 3;
+
+int notes [] = {262, 330, 392, 440}; // c,e,g,a
+
+ 
+ 
+
 float cm;
 
-
-  void setup () {
+void setup() {
 
 Serial.begin(9600); //begin serial communication
 pinMode(TrigPin, OUTPUT); //set up pin D2 for trigger pulse
 pinMode(EchoPin, INPUT); //set up pin D3 to receive echo pulse
-for (int pinNumber = 8); //Green LED light
-for (int pinNumber1 = 9);// Blue LED light
-for (int pinNumber2 = 10);// Yellow LED light
-for (int pinNumber3 = 11;)// Red LED light
+
+ pinMode (6, OUTPUT);// sound
+ pinMode (8, OUTPUT);// green LED light
+ pinMode (9, OUTPUT);// blue LED light
+ pinMode (10, OUTPUT);// yellow LED light
+ pinMode (11, OUTPUT);// red LED light
 
 }
 
-void loop() {
+void loop(){
 
 digitalWrite(TrigPin, LOW);
 delayMicroseconds(2);
@@ -30,31 +37,66 @@ Serial.print("Distance\t=\t"); //print to serial monitor
 Serial.print(cm);
 Serial.print("cm");
 Serial.println(); //jump down to next line in serial monitor
-delay(1000);
+delay(100);
 
 
 
-if (potVal <= 199) {
-    digitalWrite(3, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(9, HIGH);
+  // create an array of notes
+// the numbers below correspond to the frequencies of // middle C, D, E, and F
+
+int notes[ ] = {262, 330, 392, 440};//c,e,g,a
+
+ 
+
+ // play the note corresponding to each value on A0
+  if (cm <= 50 && cm > 40) {
+    // play the first frequency in the array on pin 8
+    tone(6, notes[0]);
+    digitalWrite (8, HIGH);
+    digitalWrite (9, LOW);
+    digitalWrite (10,LOW);
+    digitalWrite (11,LOW);
+    delay(250);
+  }
+
+
+
+else if (cm <= 40 && cm > 30) {
+    // play the second frequency in the array on pin 8
+    tone(6, notes[1]);
+    digitalWrite (8, LOW);
+    digitalWrite (9, HIGH);
+    digitalWrite (10,LOW);
+    digitalWrite (11,LOW);
+   delay(250);
+  }
+else if (cm <= 30 && cm > 20) {
+    // play the third frequency in the array on pin 8
+    tone(6, notes[2]);
+     digitalWrite (8,LOW);
+    digitalWrite (9, LOW);
+    digitalWrite (10,HIGH);
+    digitalWrite (11,LOW);
+   delay(250);
+  }
+else if (cm <= 20 && cm > 10) {
+    // play the fourth frequency in the array on pin 8
+    tone(6, notes[3]);
+    digitalWrite (8, LOW);
+    digitalWrite (9, LOW);
+    digitalWrite (10,LOW);
+    digitalWrite (11,HIGH);
+  delay(250);
   }
   
-    
-  else if( potVal >= 200 &&  potVal < 399) {
-    digitalWrite(3, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(9, HIGH);
-    
-    delay (250);
-    
-    digitalWrite(3, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(9, LOW);
-    
-    delay (250);
-    
+ 
+
+
+else {
+    // if the value is out of range, play no tone
+    noTone(8);
   }
+}
+
+
+//LED lights
